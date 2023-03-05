aa2f9957a48ea4572a1971df00462d737707b5e8
adioppo <115898990+adioppo@users.noreply.github.com>
2023-03-05 12:09:12 +0200

    Update myScript.js

--- js/myScript.js
+++ js/myScript.js
@@ -64,6 +64,7 @@ const corpul = document.getElementById("corpul");
 		document.getElementById('p_scrolabil_id').innerHTML='Capul este una dintre cele mai importante părți ale corpului uman, servind drept centru de control pentru multe funcții vitale. Este situat în partea de sus a corpului și este compus din mai multe structuri interconectate, inclusiv craniul, creierul, fața, ochii, nasul, urechile, gura și gâtul.Craniul, care protejează creierul, este structura osoasă care formează porțiunea superioară a capului. În interiorul craniului, creierul controlează diverse funcții fiziologice și cognitive, inclusiv mișcarea, senzația, gândul și emoția.Fața, situată în partea din față a capului, conține ochii, nasul și gura, care sunt esențiale pentru vedere, miros și, respectiv, gust. Urechile, situate și pe părțile laterale ale capului, ne permit să auzim și să menținem echilibrul.Gâtul conectează capul de restul corpului, oferind sprijin și facilitând mișcarea.În general, capul joacă un rol esențial în capacitatea noastră de a interacționa cu lumea din jurul nostru, iar orice deteriorare sau vătămare a acestui sistem complex poate avea consecințe grave asupra sănătății și bunăstării noastre generale.';
 		document.documentElement.scrollTop = 0;
 		document.body.scrollTop = 0;
+		document.getElementById('p_scrolabil_id').scrollTop=-50000;
 		});
 	const trunchiul = document.getElementById("trunchiul");
 	trunchiul.addEventListener("click", function() 
@@ -87,6 +88,7 @@ const corpul = document.getElementById("corpul");
 		document.getElementById('p_scrolabil_id').innerHTML='Trunchiul este o parte vitală a corpului uman, cuprinzând pieptul, abdomenul și spatele. Acesta servește ca principală structură de sprijin pentru partea superioară a corpului și găzduiește mai multe organe critice, inclusiv inima, plămânii, ficatul, stomacul și intestinele.Pieptul conține cutia toracică, care protejează inima și plămânii. Inima, un organ muscular responsabil cu pomparea sângelui în tot corpul, este situată în centrul pieptului. Plămânii, care facilitează respirația, sunt situați de ambele părți ale inimii.Abdomenul conține mai multe organe responsabile de digestie și eliminarea deșeurilor, inclusiv stomacul, ficatul și intestinele. Spatele adăpostește măduva spinării, care conectează creierul de restul corpului și susține greutatea părții superioare a corpului.Musculatura trunchiului joacă un rol crucial în mișcare și stabilitate, permițându-ne să stăm, să stăm în picioare, să mergem și să efectuăm alte activități fizice.În general, trunchiul este o parte complexă și vitală a corpului uman, esențială pentru menținerea sănătății și bunăstării noastre generale. Orice vătămare sau boală care afectează trunchiul poate avea consecințe grave asupra calității vieții noastre.';
 		document.documentElement.scrollTop = 0;
 		document.body.scrollTop = 0;
+		document.getElementById('p_scrolabil_id').scrollTop=-50000;
 		});
 	const bratele = document.getElementById("bratele");
 	bratele.addEventListener("click", function() 
@@ -109,7 +111,8 @@ const corpul = document.getElementById("corpul");
 		document.getElementById('titlul_prezentarii').innerHTML='Bratele';
 		document.getElementById('p_scrolabil_id').innerHTML='Brațele sunt părți esențiale ale corpului uman, constând din braț, antebraț, încheietură și mână. Sunt atașate de umeri și permit o gamă largă de mișcări, de la atingere și apucare până la aruncare și ridicare.Partea superioară a brațului conține humerusul, osul lung care merge de la umăr până la cot. Antebrațul conține două oase, ulna și radius, care leagă cotul de încheietura mâinii. Incheietura mainii conecteaza antebratul de mana si contine opt oase mici care permit miscari complicate. Mâna este compusă din palmă, degete și degetul mare și este esențială pentru abilitățile motorii fine, cum ar fi scrisul, tastarea și cântatul la instrumente muzicale.Mușchii brațelor permit forță și mișcare și sunt împărțiți în trei grupe: mușchii biceps, triceps și antebraț. Bicepsul și tricepsul lucrează împreună pentru a flexa și extinde cotul, în timp ce mușchii antebrațului sunt responsabili pentru mișcările încheieturii mâinii și ale mâinii.În general, brațele sunt esențiale pentru activitățile zilnice și interacțiunile cu lumea din jurul nostru, iar orice deteriorare sau rănire a acestui sistem complex poate avea consecințe grave asupra mobilității și funcționalității noastre generale.';
 		document.documentElement.scrollTop = 0;
-		document.body.scrollTop = 0;		
+		document.body.scrollTop = 0;	
+		document.getElementById('p_scrolabil_id').scrollTop=-50000;
 	});
 	const picioarele = document.getElementById("picioarele");
 	picioarele.addEventListener("click", function() 
@@ -133,5 +136,6 @@ const corpul = document.getElementById("corpul");
 		document.getElementById('p_scrolabil_id').innerHTML='Picioarele sunt o parte esențială a corpului uman, oferind sprijin, echilibru și mobilitate. Sunt formate din coapsă, genunchi, gambe, glezne și picior și sunt atașate de pelvis.Coapsa este porțiunea superioară a piciorului și conține femurul, cel mai lung os din corp. Articulația genunchiului conectează coapsa de piciorul inferior și este crucială pentru mișcare și stabilitate. Partea inferioară a piciorului conține două oase, tibia și fibula și se conectează la articulația gleznei. Piciorul este compus din gleznă, călcâi, talpă și degete și este esențial pentru a sta în picioare, a merge și a alerga.Mușchii picioarelor, inclusiv cvadricepsul, ischio-jambierii și mușchii gambei, sunt responsabili pentru mișcare și sprijin. Cvadricepsul ne permite să ne îndreptăm picioarele, în timp ce ischiobigiolarele ne permit să le îndoim. Mușchii gambei sunt esențiali pentru mers și alergare și lucrează pentru a ridica călcâiul de pe sol.În general, picioarele sunt esențiale pentru activitățile zilnice și interacțiunile cu lumea din jurul nostru, iar orice deteriorare sau vătămare a acestui sistem complex poate avea consecințe grave asupra mobilității și funcționalității noastre generale.';
 		document.documentElement.scrollTop = 0;
 		document.body.scrollTop = 0;
+		document.getElementById('p_scrolabil_id').scrollTop=-50000;
 	});
 	
